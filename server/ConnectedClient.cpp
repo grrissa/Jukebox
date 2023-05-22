be3ea8996b7b1dc714c57db6faeade681f3880b9
Marissa <marissanicoleesteban@sandiego.edu>
2023-05-22 15:07:47 -0700

    delte dummy

--- server/ConnectedClient.cpp
+++ server/ConnectedClient.cpp
@@ -28,63 +28,6 @@ namespace fs = std::filesystem;
 ConnectedClient::ConnectedClient(int fd, ClientState initial_state) :
 	client_fd(fd), sender(NULL), state(initial_state) {}
 
-void ConnectedClient::send_dummy_response(int epoll_fd) {
-	// Create a large array, just to make sure we can send a lot of data in
-	// smaller chunks.
-	char *data_to_send = new char[CHUNK_SIZE*2000];
-	memset(data_to_send, 117, CHUNK_SIZE*2000); // 117 is ascii 'u'
-
-	ArraySender *array_sender = new ArraySender(data_to_send, CHUNK_SIZE*2000);
-	delete[] data_to_send; // The ArraySender creates its own copy of the data so let's delete this copy
-
-	ssize_t num_bytes_sent;
-	ssize_t total_bytes_sent = 0;
-
-	// keep sending the next chunk until it says we either didn't send
-	// anything (0 return indicates nothing left to send) or until we can't
-	// send anymore because of a full socket buffer (-1 return value)
-	while((num_bytes_sent = array_sender->send_next_chunk(this->client_fd)) > 0) {
-		total_bytes_sent += num_bytes_sent;
-	}
-	cout << "sent " << total_bytes_sent << " bytes to client\n";
-
-	/*
-	 * TODO: if the last call to send_next_chunk indicated we couldn't send
-	 * anything because of a full socket buffer, we should do the following:
-	 *
-	 * 1. update our state field to be sending
-	 * 2. set our sender field to be the ArraySender object we created
-	 * 3. update epoll so that it also watches for EPOLLOUT for this client
-	 *    socket (use epoll_ctl with EPOLL_CTL_MOD).
-	 *
-	 * WARNING: These steps are to be done inside of the following if statement,
-	 * not before it.
-	 */
-	if (num_bytes_sent < 0) {
-		// Fill this in with the three steps listed in the comment above.
-		// WARNING: Do NOT delete array_sender here (you'll need it to continue
-		// sending later).
-		this->state = SENDING;
-		this->sender = array_sender;
-
-		// QUESTION
-		struct epoll_event epoll_out;
-        epoll_out.data.fd = this->client_fd;
-        epoll_out.events = EPOLLOUT  | EPOLLRDHUP;
-
-        if (epoll_ctl(epoll_fd, EPOLL_CTL_MOD, this->client_fd, &epoll_out) == -1) {
-            perror("send_dummy_response epoll_ctl");
-            exit(EXIT_FAILURE);
-        }
-    }
-
-
-	else {
-		// Sent everything with no problem so we are done with our ArraySender
-		// object.
-		delete array_sender;
-	}
-}
 
 void ConnectedClient::play_response(int epoll_fd, int song_num, string dir) {
 
